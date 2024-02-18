fdc5ce413902b12ff4f08ce9229292ec779cae30
BilalDeveloper900 <144093920+BilalDeveloper900@users.noreply.github.com>
2024-02-18 21:52:28 +0500

    .

--- src/index.mjs
+++ src/index.mjs
@@ -27,30 +27,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
-  console.log(request.session);
-  console.log(request.session.id);
-  request.session.visited = true;
-  response.cookie("hello", "world", { maxAge: 60000 * 60 * 2 });
   response.status(200).send("Hello, world!");
-});
-
-//......simple authentication........
-
-app.post("/api/auth", (request, response) => {
-  const {
-    body: { username, password },
-  } = request;
-  const findUser = mockUsers.find((user) => user.username === username);
-  if (!findUser || findUser.password !== password)
-    return response.status(401).send({ msg: "Bad Credentials" });
-  request.session.user = findUser;
-  return response.status(200).send(findUser);
-});
-
-app.get("/api/auth/status", (request, response) => {
-  // Not sure what you intend to do with this line
-  // request.sessionStore.get();
-  return request.session.user
-    ? response.status(200).send(request.session.user)
-    : response.status(401).send({ msg: "not authenticated" });
 });
